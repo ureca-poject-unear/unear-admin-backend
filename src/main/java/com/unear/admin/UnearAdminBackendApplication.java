1e2d19823655d656d6da8f7d195ac39448a0969f
1seyoung <akdlrnal0705@gmail.com>
2025-07-08 15:12:36 +0900

    init: initial commit

---
+++ src/main/java/com/unear/admin/UnearAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unear.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnearAdminBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnearAdminBackendApplication.class, args);
+  }
+
+}
