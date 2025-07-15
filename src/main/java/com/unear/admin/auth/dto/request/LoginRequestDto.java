b10a14b3d967b58b433ea8df07bdd1c6f997eb58
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-07-15 13:38:54 +0900

    로그인시 유효성 메시지 반환

--- src/main/java/com/unear/admin/auth/dto/request/LoginRequestDto.java
+++ src/main/java/com/unear/admin/auth/dto/request/LoginRequestDto.java
@@ -3,11 +3,16 @@ package com.unear.admin.auth.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class LoginRequestDto {
+ @Getter
+ @Setter
+ @NoArgsConstructor
+ public class LoginRequestDto {
+    @NotBlank(message = "이메일은 필수입니다")
+    @Email(message = "올바른 이메일 형식이 아닙니다")
     private String email;
+    @NotBlank(message = "비밀번호는 필수입니다")
     private String password;
-}
+ }
