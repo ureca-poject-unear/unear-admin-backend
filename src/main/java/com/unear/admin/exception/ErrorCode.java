8372f3a62c8412b29762bd65aff2441208dc4644
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-07-17 11:26:27 +0900

    refactor: code값 불일치 해결

--- src/main/java/com/unear/admin/exception/ErrorCode.java
+++ src/main/java/com/unear/admin/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "ACCESS_DENIED", "접근 권한이 없습니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "DUPLICATE_EMAIL", "이미 존재하는 이메일입니다."),
     EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "EVENT_NOT_FOUND", "이벤트 정보를 찾을 수 없습니다."),
-    COUPON_TEMPLATE_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON_NOT_FOUND", "쿠폰 정보를 찾을 수 없습니다."),
+    COUPON_TEMPLATE_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON_TEMPLATE_NOT_FOUND", "쿠폰 정보를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "서버 오류가 발생했습니다.");
 
 
