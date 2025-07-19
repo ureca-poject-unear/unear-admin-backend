af84987eeeef3d6ed7beed836b8dccfe781bd05f
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-07-19 16:48:37 +0900

    feat : 제휴처 조회 pageable 추가

--- src/main/java/com/unear/admin/places/service/PlaceService.java
+++ src/main/java/com/unear/admin/places/service/PlaceService.java
@@ -2,6 +2,8 @@ package com.unear.admin.places.service;
 
 import com.unear.admin.places.dto.requestdto.PlaceRequestDto;
 import com.unear.admin.places.dto.responsedto.PlaceResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -9,8 +11,9 @@ public interface PlaceService {
     void savePlace(Long eventId, PlaceRequestDto requestDto);
     List<PlaceResponseDto> getPartnersWithinEvent(Long eventId);
     void createPlace(PlaceRequestDto requestDto);   //crud
-    List<PlaceResponseDto> getAllPlaces();
+    Page<PlaceResponseDto> getAllPlaces(Pageable pageable);
     void deletePlace(Long PlaceId);
     void updatePlace(PlaceRequestDto requestDto);
+
 
 }
