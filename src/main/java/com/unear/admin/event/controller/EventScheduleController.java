8f94aff1e8b6fbc9ff59644f1af4143254d1384f
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-07-30 09:38:02 +0900

    refactor : 엔드포인트 개선

--- src/main/java/com/unear/admin/event/controller/EventScheduleController.java
+++ src/main/java/com/unear/admin/event/controller/EventScheduleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/events/schedule")
+@RequestMapping("admin/events/schedule")
 @RequiredArgsConstructor
 public class EventScheduleController {
 
