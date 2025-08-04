b95ef2fd985373406090401f30597179bda54f4c
Jeong Seong Bin <tjdqls3124@gmail.com>
2025-08-04 16:13:18 +0900

    refactor : 엔드포인트 수정

--- src/main/java/com/unear/admin/places/controller/PlaceController.java
+++ src/main/java/com/unear/admin/places/controller/PlaceController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/places")
+@RequestMapping("/places")
 @RequiredArgsConstructor
 public class PlaceController {
 
